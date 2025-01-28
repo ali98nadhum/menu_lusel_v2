a7928083ff705522463e534980e51c8a399ded73
Ali nadhum <alinadhum1998@gmail.com>
2025-01-28 04:40:41 +0300

    create header comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+
+import Header from "./utils/Header/Header";
+
 function App() {
   return (
     <>
+    <Header />
       <h1>hello world</h1>
     </>
   );
---
+++ src/utils/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import module from  "./header.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={module.test}>header</h1>
+    </div>
+  )
+}
+
+export default Header
