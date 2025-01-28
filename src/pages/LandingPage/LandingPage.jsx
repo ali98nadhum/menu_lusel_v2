67f18882bb0265a533bfd8bcb56a470490cc06c3
Ali nadhum <alinadhum1998@gmail.com>
2025-01-28 06:16:39 +0300

    dev 50% from landingPage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import module from "./landingPage.module.css";
 import Container from '@mui/material/Container';
+import Lottie from "lottie-react";
+import coffeeAnimation  from "../../../public/coffee-Animation.json";
+import Button from '@mui/material/Button';
 
 const LandingPage = () => {
   return (
     <div className={module.landingPage}>
      <Container>
-        
+        {/* ----- Animation ------- */}
+        <div className={module.AnimationCoffee}>
+            <Lottie loop={false} animationData={coffeeAnimation} style={{height: "250px"}} />
+        </div>
+        {/* ------- Button menu ------- */}
+        <div className={module.btn_menu}>
+        <Button variant="contained">المنيو</Button>
+        </div>
      </Container>
     </div>
   )
