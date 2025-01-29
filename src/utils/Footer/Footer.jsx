dfcd40d78668d736c9e32de4bea36e7398ad2975
Ali nadhum <alinadhum1998@gmail.com>
2025-01-29 04:44:15 +0300

    create footer steksher

--- src/utils/Footer/Footer.jsx
+++ src/utils/Footer/Footer.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react";
 import module from "./footer.module.css";
+import Container from "@mui/material/Container";
+import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
-}
+    <footer>
+      <Container>
+        <div className={module.top}>
+          <div className={module.left_sec}>
+            <h3>ss</h3>
+            <p>f</p>
+          </div>
+          <div className={module.right}>
+            <h2></h2>
+            <div className={module.icon_side}>
+                <a href="https://instagram.com/lusail_cafe?igshid=MTNiYzNiMzkwZA=="><FaInstagram className="insta-icon" /></a>
+                <a href="tel:+7705985657"><FaWhatsapp  className="whats-icon"/></a>
+            </div>
+          </div>
+        </div>
 
-export default Footer
+        <div className={module.bottom}></div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
