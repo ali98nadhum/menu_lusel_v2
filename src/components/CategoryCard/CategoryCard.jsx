a2ba3f4c1cab23b9de50013b133fc8c4e44b3cc2
Ali nadhum <alinadhum1998@gmail.com>
2025-01-29 18:02:38 +0300

    create categoryCard

---
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import module from "./categorycard.module.css";
+
+const CategoryCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryCard
--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import module from "./category.module.css";
+import foodAnimation from "../../../public/foodAnimation.json";
+import Lottie from "lottie-react";
+import { Container } from '@mui/material';
 
 const CategoryPage = () => {
   return (
     <div className={module.CategoryPage}>
-      <h1>الاصناف</h1>
+        <Container>
+            {/* ---- category header ------- */}
+      <div className={module.category_header}>
+        <Lottie
+            loop={true}
+            animationData={foodAnimation}
+            style={{ height: "80px" }}
+          />
+           <h1>قائمه الطعام</h1>
+      </div>
+      <hr className={module.line} />
+      {/* ------- category card ------- */}
+      <div className={module.grid}></div>
+      </Container>
     </div>
   )
 }
