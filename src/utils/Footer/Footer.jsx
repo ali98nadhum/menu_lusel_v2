150d363d77f73d311110f2e8d2ddb64ff6836936
Ali nadhum <alinadhum1998@gmail.com>
2025-01-29 05:04:34 +0300

    end top and button sec in footer

--- src/utils/Footer/Footer.jsx
+++ src/utils/Footer/Footer.jsx
@@ -10,14 +10,14 @@ const Footer = () => {
       <Container>
         <div className={module.top}>
           <div className={module.left_sec}>
-            <h3>ss</h3>
-            <p>f</p>
+            <h3>Lusail Cafe</h3>
+            <p>Lusail cafe is that specialist cafe in iraq</p>
           </div>
-          <div className={module.right}>
-            <h2></h2>
+          <div className={module.right_sec}>
+            <h3>للتواصل</h3>
             <div className={module.icon_side}>
-                <a href="https://instagram.com/lusail_cafe?igshid=MTNiYzNiMzkwZA=="><FaInstagram className="insta-icon" /></a>
-                <a href="tel:+7705985657"><FaWhatsapp  className="whats-icon"/></a>
+                <a href="https://instagram.com/lusail_cafe?igshid=MTNiYzNiMzkwZA=="><FaInstagram className={module.insta_icon} /></a>
+                <a href="tel:+7705985657"><FaWhatsapp  className={module.whats_icon}/></a>
             </div>
           </div>
         </div>
