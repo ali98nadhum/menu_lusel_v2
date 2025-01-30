64ffd4334c16ae1d8775f281bfaea9e7616fc2c1
Ali nadhum <alinadhum1998@gmail.com>
2025-01-30 17:15:39 +0300

    end from productCard

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -3,8 +3,16 @@ import module from "./productCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div>
-      
+    <div className={module.menu}>
+        <div className={module.menu_item}>
+            <img src="https://static.vecteezy.com/system/resources/previews/022/911/694/non_2x/cute-cartoon-burger-icon-free-png.png" alt="برجر لحم" />
+            <div className={module.item_info}>
+                <h3>برجر لحم</h3>
+                <p>لحم مشوي مع الجبن والخضار الطازجة</p>
+                <span className={module.price}>4500 IQD</span>
+            </div>
+        </div>
+
     </div>
   )
 }
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import module from "./productPage.module.css";
 import { Container } from '@mui/material';
+import ProductCard from '../../components/productCard/ProductCard';
 
 function ProductPage() {
   return (
@@ -10,6 +11,10 @@ function ProductPage() {
         <div className={module.productHeader}>
             <h1>🍽️ البركر</h1>
             <hr/>
+        </div>
+        {/* ------ Product sec -------- */}
+        <div className={module.productSec}>
+            <ProductCard />
         </div>
      </Container>
     </div>
