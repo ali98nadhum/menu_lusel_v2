3446bd4508abb166615887bd52d2843b492c489a
Ali nadhum <alinadhum1998@gmail.com>
2025-01-30 17:00:06 +0300

    create productCard

---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import module from "./productCard.module.css";
+
+const ProductCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductCard
