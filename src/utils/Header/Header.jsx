78cb76e43737d14d70a8187e4a6ef523de5bc775
Ali nadhum <alinadhum1998@gmail.com>
2025-02-01 07:40:44 +0300

    edit header.jsx

--- src/utils/Header/Header.jsx
+++ src/utils/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import module from  "./header.module.css";
 import logo from "../../assets/nav-logo.png";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
    <div className={module.Header}>
         <div className={module.logo}>
-            <img src={logo} alt="logo" />
+            <Link to="/"> <img src={logo} alt="logo" /> </Link>
         </div>
    </div>
   )
